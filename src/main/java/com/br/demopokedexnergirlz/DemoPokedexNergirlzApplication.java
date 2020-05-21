a7d4117d88b77a208a0a7823789f55aa245b29df
anabneri <$git_config_user_email>
2020-05-21 15:29:09 -0300

    :zap: first commit

---
+++ src/main/java/com/br/demopokedexnergirlz/DemoPokedexNergirlzApplication.java
@@ -0,0 +1,14 @@
+package com.br.demopokedexnergirlz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPokedexNergirlzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPokedexNergirlzApplication.class, args);
+		System.out.println("Helo Pokedex");
+	}
+
+}
